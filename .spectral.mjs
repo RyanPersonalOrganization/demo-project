84899005abbdb81be26724e200b1735df8515889
ryan.grantom@stoplight.io <ryan.grantom@stoplight.io>
2023-02-23 13:27:53 -0600

    Modified 16 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4Njc3Mg/spectral.js?branch_slug=main&token=d5403c45-b169-4394-971c-8832f1bea388";
+export default { extends: ruleset };
